d0acc028692273c9d05a62ca5b365fc220bd9336
augustinlss <augustin.zhlass@gmail.com>
2025-10-05 00:30:10 +0200

    added TokenResponse type

--- internal/msgraph/msgraph.go
+++ internal/msgraph/msgraph.go
@@ -6,3 +6,13 @@ type Client struct {
 	RedirectURI  string
 	Scopes       []string
 }
+
+type TokenResponse struct {
+	TokenType    string `json:"token_type"`
+	Scope        string `json:"scope"`
+	ExpiresIn    int    `json:"expires_in"`
+	ExtExpiresIn int    `json:"ext_expires_in"`
+	AccessToken  string `json:"access_token"`
+	RefreshToken string `json:"refresh_token"`
+	IDToken      string `json:"id_token"`
+}
